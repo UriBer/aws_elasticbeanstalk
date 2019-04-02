73f64a31d6d92fe7a85d09f4153ac504f7ee19d6
EC2 Default User <ec2-user@ip-172-31-86-236.ec2.internal>
2019-04-02 14:14:30 +0000

    added currency extraction

--- application.py
+++ application.py
@@ -13,6 +13,18 @@ def post_root():
     return Response(json.dumps({'Output': 'Hello Post World'}), mimetype='application/json', status=200)
 
 
+@application.route('/calc/currency/<string:currency>', methods=['POST'])
+def post_currency(currency):
+    res = currency_rate.get(currency, 0.00) 
+    return Response(json.dumps({currency: res}), mimetype='application/json', status=200)
+
+
+currency_rate = {
+    'usd' : 3.3,
+    'pound' : 4.5
+}
+
+
 if __name__ == "__main__":
     application.debug = True
     application.run()
