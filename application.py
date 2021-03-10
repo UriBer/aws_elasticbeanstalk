7306afe84c34a526453fee963e283b776f5515ee
EC2 Default User <ec2-user@ip-172-31-27-150.ec2.internal>
2021-03-10 17:09:16 +0000

    added chilki route

--- application.py
+++ application.py
@@ -13,6 +13,11 @@ def post_root():
     return Response(json.dumps({'Output': 'Hello Post World'}), mimetype='application/json', status=200)
 
 
+@application.route('/chilki', methods=['POST'])
+def post_req():
+    return Response(json.dumps({'Output': 'Hello chilki '}), mimetype='application/json', status=200)
+
+
 if __name__ == "__main__":
     application.debug = True
     application.run()
